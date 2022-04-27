496a2029045ca7785566d0bb895ceeaded6a546d
Kc <40486366+cpwillis@users.noreply.github.com>
2022-04-28 09:55:59 +1000

    maze creation dialogue

--- src/MazeDesignTool/GUI.java
+++ src/MazeDesignTool/GUI.java
@@ -10,7 +10,7 @@ public class GUI extends JFrame implements ActionListener {
 
     public static final int width = 800;
     public static final int height = 700;
-    JMenuItem mNew, mOpen, mSave, mExit, mImportImage, mHowTo, mAbout;
+    JMenuItem mNew, mOpen, mSave, mExit, mImport, mHowTo, mAbout;
     JFrame masterWindow = new JFrame("Maze Design Tool (Group_282)");
 
     private void initFrame() {
@@ -28,37 +28,24 @@ public class GUI extends JFrame implements ActionListener {
         JMenuBar menuBar = new JMenuBar();
 
         // Create Options
-        mNew = new JMenuItem("New");
-        mNew.addActionListener(this);
-        mOpen = new JMenuItem("Open");
-        mOpen.addActionListener(this);
-        mSave = new JMenuItem("Save");
-        mSave.addActionListener(this);
-        mExit = new JMenuItem("Exit");
-        mExit.addActionListener(this);
-        mImportImage = new JMenuItem("Import Image");
-        mImportImage.addActionListener(this);
-        mHowTo = new JMenuItem("How To?");
-        mHowTo.addActionListener(this);
-        mAbout = new JMenuItem("About");
-        mAbout.addActionListener(this);
+        mNew = new JMenuItem("New"); mNew.addActionListener(this);
+        mOpen = new JMenuItem("Open"); mOpen.addActionListener(this);
+        mSave = new JMenuItem("Save"); mSave.addActionListener(this);
+        mExit = new JMenuItem("Exit"); mExit.addActionListener(this);
+        mImport = new JMenuItem("Import Image"); mImport.addActionListener(this);
+        mHowTo = new JMenuItem("How To?"); mHowTo.addActionListener(this);
+        mAbout = new JMenuItem("About"); mAbout.addActionListener(this);
 
         // Create Heading and Add Respective Options
         JMenu mFile = new JMenu("File");
-        mFile.add(mNew);
-        mFile.add(mOpen);
-        mFile.add(mSave);
-        mFile.add(mExit);
+        mFile.add(mNew); mFile.add(mOpen); mFile.add(mSave); mFile.add(mExit);
         JMenu mEdit = new JMenu("Edit");
-        mEdit.add(mImportImage);
+        mEdit.add(mImport);
         JMenu mHelp = new JMenu("Help");
-        mHelp.add(mHowTo);
-        mHelp.add(mAbout);
+        mHelp.add(mHowTo); mHelp.add(mAbout);
 
         // Attach Headings to MenuBar
-        menuBar.add(mFile);
-        menuBar.add(mEdit);
-        menuBar.add(mHelp);
+        menuBar.add(mFile); menuBar.add(mEdit); menuBar.add(mHelp);
 
         // Return
         return menuBar;
@@ -72,20 +59,45 @@ public class GUI extends JFrame implements ActionListener {
     }
 
     private void createMaze() {
-        // todo: maze creation dialogue for user to pick options (e.g. size)
-        Maze tst = new Maze(5,5,"Tester", "Chris");
-        viewMaze(tst);
+        JDialog MazeDialogue = new JDialog();
+        MazeDialogue.setLayout(new GridBagLayout());
+        MazeDialogue.setSize(200,200);
+        MazeDialogue.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        MazeDialogue.setLocationRelativeTo(null);
+
+        String[] MazeProperties = {"Title", "Width", "Height", "Author"};
+        int i = 0;
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 0.5; c.fill = GridBagConstraints.HORIZONTAL;
+        for (String x: MazeProperties) {
+            JLabel abc = new JLabel(x+":");
+            c.gridx = 0; c.gridy = i;
+            MazeDialogue.add(abc, c);
+
+            JTextField def = new JTextField();
+            c.gridwidth = 2; c.ipadx = 60; c.gridx = 1; c.gridy = i;
+            MazeDialogue.add(def, c);
+            i++;
+        }
+        JButton submit = new JButton("Submit");
+        c.weightx = 0.0; c.gridwidth = 3; c.gridx = 0; c.gridy = i;
+        MazeDialogue.add(submit, c);
+        submit.addActionListener(e -> {
+            //todo: use values from dialogue to generate maze
+            Maze HardcodedMaze = new Maze(5,5,"Tester", "Chris"); //TESTER
+            viewMaze(HardcodedMaze);
+            MazeDialogue.dispose();
+        });
+        MazeDialogue.setVisible(true);
     }
 
-    private void viewMaze(Maze currentMaze) {
-        JInternalFrame slaveWindow = new JInternalFrame(currentMaze.getWindowName()); // Sits Inside masterWindow
+    public void viewMaze(Maze currentMaze) {
+        JInternalFrame slaveWindow = new JInternalFrame(currentMaze.getWindowName(), true, true); // Sits Inside masterWindow
         slaveWindow.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         slaveWindow.setLayout(new BorderLayout());
-        slaveWindow.setClosable(true);
         slaveWindow.setVisible(true);
-        slaveWindow.add(new JLabel("If you see me, maze didn't paint.")); //TESTER
-        slaveWindow.add(new DrawMaze()); // todo: draw maze
-        masterWindow.getContentPane().add(slaveWindow, BorderLayout.CENTER);
+        slaveWindow.add(new DrawMaze());
+        masterWindow.getContentPane().add(slaveWindow);
         masterWindow.revalidate();
     }
 
@@ -95,7 +107,7 @@ public class GUI extends JFrame implements ActionListener {
         if (e.getSource() == mOpen) { fileDirectory(); }
         if (e.getSource() == mSave) { fileDirectory(); }
         if (e.getSource() == mExit) { System.exit(0); }
-        if (e.getSource() == mImportImage) { } // start and finish images
+        if (e.getSource() == mImport) { } // start and finish images
         if (e.getSource() == mHowTo) { } // what it does/how
         if (e.getSource() == mAbout) { } // project name/course/year, team members
     }
